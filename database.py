eaea36f03529e81407aef52acb27fb100aa7a888
Lucas Martins <dev.lucasmartins@gmail.com>
2019-04-05 10:16:47 -0300

    added application_id property

--- database.py
+++ database.py
@@ -15,8 +15,9 @@ def find_subscription(username):
     })
 
 
-def save_subscription(username, subscription_info):
+def save_subscription(username, application_id, subscription_info):
     return subscriptions_collection.insert_one({
         "username": username,
+        "application_id": application_id,
         "subscription_info": subscription_info
     })
