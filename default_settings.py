48a79a987cb44da77d5e17ff709d5507cb71da73
Lucas Martins <peo_lmartins@uolinc.com>
2021-01-31 15:18:42 -0300

    adjustment to JWT_PUBLIC_KEY

--- default_settings.py
+++ default_settings.py
@@ -7,5 +7,3 @@ SECRET_KEY = binascii.hexlify(os.urandom(24))
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# OAuth2 JWT Public Key
-JWT_PUBLIC_KEY = os.getenv("JWT_PUBLIC_KEY", "")
